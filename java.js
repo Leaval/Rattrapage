73af57680e09607b481502d74019f56feb9ea800
Leaval <leaaguede.2003@gmail.com>
2023-06-14 13:45:37 +0200

    first commit

---
+++ java.js
@@ -0,0 +1,60 @@
+function formulaire(){
+    var TVA = document.getElementById("TVA")
+   if(TVA.value.length == 0){
+       alert("le champ tva est vide") 
+   }
+   }
+   function formulaire(){
+    var Prenom = document.getElementById("Prenom")
+   if(Prenom.value.length == 0){
+       alert("le champ Prenom est vide") 
+   }
+   }
+   function formulaire(){
+    var nom = document.getElementById("nom")
+   if(nom.value.length == 0){
+       alert("le champ nom est vide") 
+   }
+   }
+   function formulaire(){
+    var email = document.getElementById("email")
+   if(email.value.length == 0){
+       alert("le champ email est vide") 
+   }
+   }
+   function formulaire(){
+    var tel = document.getElementById("tel")
+   if(tel.value.length == 0){
+       alert("le champ telephone est vide") 
+   }
+   }
+   function formulaire(){
+    var adresse = document.getElementById("adresse")
+   if(adresse.value.length == 0){
+       alert("le champ adresse est vide") 
+   }
+   }
+   function formulaire(){
+    var region = document.getElementById("region")
+   if(region.value.length == 0){
+       alert("le champ region est vide") 
+   }
+   }
+   function formulaire(){
+    var ville = document.getElementById("ville")
+   if(ville.value.length == 0){
+       alert("le champ ville est vide") 
+   }
+   }
+   function formulaire(){
+    var code = document.getElementById("code")
+   if(code.value.length == 0){
+       alert("le champ code postale est vide") 
+   }
+   }
+   function formulaire(){
+    var carte = document.getElementById("carte")
+   if(carte.value.length == 0){
+       alert("le champ carte de paiement est vide") 
+   }
+   }
